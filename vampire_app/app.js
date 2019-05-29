a0e0ee6f09531f436bf88296f46de6db28bff866
Tommie <tommiealcorn@gmail.com>
2019-05-29 07:34:33 -0600

    get everything into my master branch

--- vampire_app/app.js
+++ vampire_app/app.js
@@ -4,13 +4,47 @@ const app            = express();
 const bodyParser     = require('body-parser');
 const methodOverride = require('method-override')
 const mongoose       = require('mongoose');
-// require our db file
-require('./db/db');
+
+
 // 2. Require your model (and possibly your extra data source);
-const vampire_app    = require(populateVampires);
+const vampire        = require('./models/vampire.js');
+const vampireData    = require('./db/populateVampires.js');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
+
+app.use(session({
+    secret: 'This is a random secret string that you would make up to protect your session',
+    resave: false, 
+    saveUninitialized: false 
+  }))
+
+
+
+
 // 3. Connect your database and collection name
+require('./db/db');
 
 // 4. Open your mongoose connection
+
+mongoose.connect('mongodb://localhost/vampires', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+});
+
+mongoose.connection.on('connected', ()=> {
+    console.log(`Mongoose connect to ${connectionString}`);
+});
+
+mongoose.connection.on('disconnected', () => {
+    console.log(`Mongoose diconnected from ${connectionString}`);
+});
+
+mongoose.connection.on('error', (err) => {
+    console.log(`Mongoose err: ${err}`);
+});
+
+
 
 /////////////////////////////////////////////////
 //Write your answers to add, query, update, remove, and Hungry for More below.
@@ -20,6 +54,27 @@ const vampire_app    = require(populateVampires);
 /////////////////////////////////////////////////
 // INSERT USING MONGOOSE
 // ### Add the vampire data that we gave you
+// vampire.collection.insertMany(vampireInfo, (err, data) => {
+//     console.log(vampireInfo);
+//     mongoose.connection.close();
+// });
+// ### Add some new vampire data
+vampire.create({
+    name: 'Tommie',
+    hair_color: 'black',
+    eye_color: 'hazel',
+    dob: new Date(1964, 5, 02, 7, 47),
+    loves: 'sleeping',
+    location: 'Denver, Colorado', 
+    gender: 'm',
+    victims: '22'
+}, (err, newVamp) => {
+    if(err){
+        console.log(err);
+    } else {
+        console.log(newVamp);
+    }
+});
 
 // ### Add some new vampire data
 
@@ -27,15 +82,46 @@ const vampire_app    = require(populateVampires);
 // ## QUERYING
 /////////////////////////////////////////////////
 // ### Select by comparison
-
+//find all vampires with less than 1000 victims
+// vampire.find({victims: {$lt: '1000'}}, 
+// (err, vampsWithouManyKills) => {
+// if(err){
+//     console.log(err);
+// }else{
+//     console.log(vampsWithouManyKills);
+// }
+// });
 /////////////////////////////////////////////////
 // ### Select by exists or does not exist
+//  vampire.find({title: {$exists: true}}, (err, vampsWithTitles)=>{
+//     if(err){
+//         console.log(err);
+//     } else { 
+//         console.log(vampsWithTitles);
+//     }
+// });
 
 /////////////////////////////////////////////////
 // ### Select with OR
+// vampire.find({$or: [{eye_color: 'hazel'}, {hair_color: 'black'}] },
+// (err,vampsWithDiffs) => {
+//     if(err){
+//         console.log(err);
+//     } else { 
+//         console.log(vampsWithDiffs);
+//     }
+// });
 
 /////////////////////////////////////////////////
 //### Select objects that match one of several values
+// vampire.find({name: {$exists: true}}, 
+//   (err, vampNamedSteve) => {
+//       if(err) {
+//           console.log(err);
+//       } else {
+//           console.log(vampNamedSteve);
+//       }
+//   })
 
 /////////////////////////////////////////////////
 //### Negative Selection
--- vampire_app/models/vampire.js
+++ vampire_app/models/vampire.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose');
 
 const vampireSchema = new mongoose.Schema({
-    name: { type: String, required: [true, "You must name your Vampire"]},
-    hair_color: { type: String, default: "blonde"},
-    eye_color: String,
-    dob: Date,
-    loves: String,
-    Location: String,
-    gender: String,
-    victims: { type: Number, min: 0},
+    name: {type: String, required: [true, "You must name your Vampire"]},
+    hair_color: {type: String, default: "blonde"},
+    eye_color: {type: String},
+    dob: {type: Date},
+    loves: [String],
+    Location: {type: String},
+    gender: {type: String},
+    victims: {type: Number, min: [0, 'Cannot have a negative number of victims']},
 });
 
 const Vampire = mongoose.model('Vampire', vampireSchema);
